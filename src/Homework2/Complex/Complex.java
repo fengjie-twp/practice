de76cca6a8922f18113a7c68b140fffdc9b7dce7
unknown <fengjie332015@icloud.com>
2026-04-18 19:53:17 +0800

    第一版完成，待测试

--- src/Homework2/Complex/Complex.java
+++ src/Homework2/Complex/Complex.java
@@ -3,7 +3,7 @@ package Homework2.Complex;
 public class Complex {
     private double real;
     private double imag;
-
+    public static  double EPSILON = 1e-8;
     public Complex() {
 
     }
@@ -35,7 +35,7 @@ public class Complex {
     }
 
     public String toString(){
-        return real + imag + "i";
+        return "("+ real + " + " + imag + "i)";
     }
 
     public boolean isReal(){
@@ -46,4 +46,48 @@ public class Complex {
         return (real == 0 && imag != 0);
     }
 
+    public boolean equals(double real, double imag){
+        if (Double.isNaN(real) || Double.isNaN(imag))
+            return false;
+        return (Math.abs(this.real - real) < EPSILON)
+                && (Math.abs(this.imag - imag) < EPSILON);
+    }
+
+    public boolean equals(Complex another){
+        return (Math.abs(this.real - another.real) < EPSILON)
+                && (Math.abs(this.imag - another.imag) < EPSILON);
+    }
+
+    public double abs(){
+        return Math.sqrt(Math.pow(this.real, 2) + Math.pow(this.imag, 2));
+    }
+
+    public Complex add(Complex right){
+        Complex c;
+        c.real = this.real + right.real;
+        c.imag = this.imag + right.imag;
+        return c;
+    }
+
+    public Complex subtract(Complex right){
+        Complex c;
+        c.real = this.real - right.real;
+        c.imag = this.imag - right.imag;
+        return c;
+    }
+
+    public Complex multiply(Complex right){
+        Complex c;
+        c.real = this.real * right.real - this.imag * right.imag;
+        c.imag = this.real * right.imag + this.imag * right.real;
+        return c;
+    }
+
+    public Complex divide(Complex right){
+        Complex c;
+        double mo = this.abs();
+        c.real = (this.real * right.real + this.imag * right.imag) / Math.pow(mo, 2);
+        c.imag = (this.real * right.imag - this.imag * right.real) / Math.pow(mo, 2);
+        return c;
+    }
 }
---
+++ src/Homework2/Complex/TestComplex.java
@@ -0,0 +1,5 @@
+package Homework2.Complex;
+
+public class TestComplex {
+
+}
